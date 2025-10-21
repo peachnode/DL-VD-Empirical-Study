4ed0e1f71d172fc7a6e889563f488be793dcde69
araiwa <contact@peachno.de>
2025-10-21 16:56:21 +0200

    remove reval

--- ReVeal-master/Vuld_SySe/representation_learning/baseline_devign.sh
+++
@@ -1,6 +0,0 @@
-#python api_test.py --dataset devign --features wo_ggnn --lambda1 0 --lambda2 0  --baseline;
-#python api_test.py --dataset devign --features ggnn --lambda1 0 --lambda2 0  --baseline;
-#python api_test.py --dataset devign --features wo_ggnn --lambda1 0 --lambda2 0  --baseline --baseline_balance  --baseline_model lr;
-python api_test.py --dataset devign --features ggnn --lambda1 0 --lambda2 0  --baseline  --baseline_balance  --baseline_model lr;
-#python api_test.py --dataset devign --features wo_ggnn --lambda1 0 --lambda2 0  --baseline --baseline_balance  --baseline_model rf;
-#python api_test.py --dataset devign --features ggnn --lambda1 0 --lambda2 0  --baseline  --baseline_balance  --baseline_model rf;
--- ReVeal-master/scripts/draper_test_on_chrome_deb.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH='../Vuld_SySe/':$PYTHONPATH
-python ../Vuld_SySe/draper_main.py \
-	--train_file ../data/draper/train_sampled.json \
-	--test_file ../data/draper/chrome_debian.json \
-	--model_path ../models/draper/test_chrome_debian.bin --test_only >> ../outputs/draper_test_chrome_debian.txt
